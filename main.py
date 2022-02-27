28b19f8091d70d10aade6e1468b4cf5de30f4b8a
Sreeram Aj <sreeramzeno@gmail.com>
2022-02-27 22:49:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,27 +30,31 @@ def home_page():
         for char in Title:
             if char != '"' and char != "'":
               new_string +=char
-        def deEmojify(new_string):
-            regrex_pattern = re.compile(pattern = "["
-                u"\U0001F600-\U0001F64F"
-                u"\U0001F300-\U0001F5FF" 
-                u"\U0001F680-\U0001F6FF" 
-                u"\U0001F1E0-\U0001F1FF"  
-                u"\U00002702-\U000027B0"
-                u"\U000024C2-\U0001F251"
-                u"\U0001f926-\U0001f937"
-                u'\U00010000-\U0010ffff'
-                u"\u200d"
-                u"\u2640-\u2642"
-                u"\u2600-\u2B55"
-                u"\u23cf"
-                u"\u23e9"
-                u"\u231a"
-                u"\u3030"
-                u"\ufe0f"
-                            "]+", flags = re.UNICODE)
-            return regrex_pattern.sub(r'',new_string)
-        Video_Name = deEmojify(new_string)
+#uncomment if you do not need emojis in the youtube video title
+
+#         def deEmojify(new_string):
+#             regrex_pattern = re.compile(pattern = "["
+#                 u"\U0001F600-\U0001F64F"
+#                 u"\U0001F300-\U0001F5FF" 
+#                 u"\U0001F680-\U0001F6FF" 
+#                 u"\U0001F1E0-\U0001F1FF"  
+#                 u"\U00002702-\U000027B0"
+#                 u"\U000024C2-\U0001F251"
+#                 u"\U0001f926-\U0001f937"      
+#                 u'\U00010000-\U0010ffff'
+#                 u"\u200d"
+#                 u"\u2640-\u2642"
+#                 u"\u2600-\u2B55"
+#                 u"\u23cf"
+#                 u"\u23e9"
+#                 u"\u231a"
+#                 u"\u3030"
+#                 u"\ufe0f"
+#                             "]+", flags = re.UNICODE)
+#             return regrex_pattern.sub(r'',new_string)
+#         Video_Name = deEmojify(new_string)
+
+Video_Name = new_string
 
     def main():
        (Video_Name,Video_Link)
